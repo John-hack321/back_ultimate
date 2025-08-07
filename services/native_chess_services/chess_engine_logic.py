961d1c2074ddf7a4c0c352bb9a4f4283aa68893e
johtieno6@gmail.com <johtieno6@gmail.com>
2025-08-07 22:30:09 +0300

    this is the start at building this into the backend

--- services/native_chess_services/chess_engine_logic.py
+++ services/native_chess_services/chess_engine_logic.py
@@ -1,6 +1,18 @@
 import chess
 import chess.engine
 import asyncio
+from dotenv import load_dotenv
+import logging
+import os
+
+load_dotenv()
+logger = logging.getLogger(__name__)
 
 class NativeChessEngine:
+
+    engine_binary_url = os.getenv('NATIVE_CHESS_BINARY_PATH')
+
     def __init__ (self):
+        native_engine_url = self.engine_binary_url
+
+    
